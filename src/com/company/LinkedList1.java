a9d436c2b916561d8b102f24f5fd30d922a48d8d
LYV\christian.hernandez <christian.hernandez@ticketmaster.com>
2017-04-13 15:26:16 -0700

    First Commit with the basic structure for a linked list.

---
+++ src/com/company/LinkedList1.java
@@ -0,0 +1,52 @@
+package com.company;
+
+/**
+ * Created by christian.hernandez on 4/13/2017.
+ */
+public class LinkedList1 {
+
+    protected Node head;
+    int size;
+
+    public LinkedList1() {
+        size = 0;
+
+    }
+
+    //adds a new node at the end of the list)(tail)
+    public void add(Node n) {
+
+    }
+
+    //adds a new node at the specified index
+    public void add(Node n, int index) {
+
+    }
+
+    public void set(Node n,int index){
+
+    }
+
+    //deletes the node at the beginning of the list (head)
+    public void delete() {
+
+    }
+
+    //deletes the node at the specified index
+    public void delete(Node n, int index) {
+
+    }
+
+    public String get(int index) {
+        return "";
+    }
+
+
+    public int size() {
+        return size;
+    }
+
+    public void clear() {
+
+    }
+}
---
+++ src/com/company/Node.java
@@ -0,0 +1,19 @@
+package com.company;
+
+/**
+ * Created by christian.hernandez on 4/13/2017.
+ */
+public class Node {
+
+    protected String node_data;
+    protected Node next;
+
+    //constructor
+    public Node(String data){
+        node_data=data;
+        next = null;
+
+    }
+
+    //TODO: create remaining methods for nodes that can't/shouldn't be implemented at the linked list level.
+}
